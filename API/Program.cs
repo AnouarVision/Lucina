5351ee1212e466c86688c71fb4740418355a882a
Anouar Naouri <102242949+AnouarVision@users.noreply.github.com>
2025-07-31 19:06:15 +0200

    Add repository pattern for Product entity with filtering and sorting
    - Created IProductRepository interface and its implementation
    - Implemented repository methods: GetProductsAsync, GetProductByIdAsync, etc.
    - Added support for filtering by brand and type and sorting products
    - Implemented seeding logic for Product data
    - Exposed brand and type endpoints in ProductsController

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using Infrastructure.Data;
+using Core.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,10 +12,26 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+// Adds ProductRepository as the implementation of IProductRepository with a scoped lifetime.
+// A new instance will be created per HTTP request.
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.MapControllers();
+
+try{
+	using var scope = app.Services.CreateScope();
+	var services = scope.ServiceProvider;
+	var context = services.GetRequiredService<StoreContext>();
+	await context.Database.MigrateAsync();
+	await StoreContextSeed.SeedAsync(context);
+}
+catch (System.Exception ex) {
+	Console.WriteLine(ex);
+	throw;
+}
 
 app.Run();
---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+
+namespace Core.Interfaces;
+    
+public interface IProductRepository
+{
+	//Retrieves a read-only list of products, filtered by brand and/or type.
+	Task<IReadOnlyList<Product>> GetProductsAsync(string? brand, string? type, string? sort);
+    Task<Product?> GetProductByIdAsync(int id);
+	Task<IReadOnlyList<string>> GetBrandsAsync();
+	Task<IReadOnlyList<string>> GetTypesAsync();
+    void AddProduct(Product product);
+    void UpdateProduct(Product product);   
+    void DeleteProduct(Product product);
+    bool ProductExists(int id);
+    Task<bool> SaveChangesAsync();
+}
