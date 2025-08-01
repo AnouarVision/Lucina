37e7505c3f2c3a93fec37e27a8e68b875be531c6
Anouar Naouri <102242949+AnouarVision@users.noreply.github.com>
2025-08-01 20:03:10 +0200

    Implement product specification parameters, add pagination support, create base API controller and integrate search functionality

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -10,6 +10,18 @@ public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecif
     public Expression<Func<T, object>>? OrderBy { get; private set; }
     public Expression<Func<T, object>>? OrderByDescending { get; private set; }
     public bool IsDistinct { get; private set; }
+	public int Take {get; private set;}
+	public int Skip {get; private set;}
+	public bool IsPagingEnabled {get; private set;}
+
+	public IQueryable<T> ApplyCriteria(IQueryable<T> query)
+	{
+		if(Criteria != null){
+			query = query.Where(Criteria);
+		}
+		
+		return query;
+	}
 
     protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
     {
@@ -25,6 +37,12 @@ public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecif
     {
         IsDistinct = true;
     }
+
+	protected void ApplyPaging(int skip, int take){
+		Skip = skip;
+		Take = take;
+		IsPagingEnabled = true;
+	}
 }
 
 public class BaseSpecification<T, TResult>(Expression<Func<T, bool>> criteria)
